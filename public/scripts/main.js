a076c428b7352048ee5440b179c249bf2ecbce23
LucasFR95 <lucasfr@outlook.com.br>
2021-06-26 22:52:03 -0300

    Projeto final

---
+++ public/scripts/main.js
@@ -0,0 +1,21 @@
+import Modal from './modal.js'
+
+const modal = Modal();
+
+const modalTitle = document.querySelector('.modal h2');
+
+const modalDescription = document.querySelector('.modal p');
+
+const checkButtons = document.querySelectorAll(".actions a.check");
+
+checkButtons.forEach(button => {
+    button.addEventListener("click", event => {modal.open()})
+})
+
+const deleteButtons = document.querySelectorAll(".actions a.delete")
+
+deleteButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        modal.open()
+    })
+})
